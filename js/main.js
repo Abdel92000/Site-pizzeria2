8590634e4d33ac9578abb44b94f2e554bd51fc08
Abdel92000 <abdelghani.rahal123@gmail.com>
2024-11-05 09:38:27 +0100

    ajout de modif

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+    const textBienvenue = document.querySelector('#section-welcome h1');
+    const image = document.querySelector('#section-welcome img');
+
+    
+    function elementOut() {
+        textBienvenue.style.animation = 'slideOutLeft 2s forwards';
+        image.style.animation = 'slideOutRight 2s forwards';
+    }
+
+   
+    setTimeout(elementOut, 2000);
+    
